8892d061e0eea87aeefb9e4a9f1f640f3e133d5e
Noah Lc <noah.source@gmail.com>
2020-01-23 20:29:53 +0100

    combine styles to a single style with amp custom, make the index a valid amp.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,25 @@
 
+const modifyHtml = (html) => {
+  // Remove newlines
+  html = html.replace(/\r?\n|\r/g, '')
+  // Combine css into single tag
+  let styleConcat = ''
+  html = html.replace(/<style[^>]*data-vue-ssr[^>]*>(.*?)?<\/style>/gi, (match, sub) => {
+    styleConcat += sub.replace(/\[.*?\]/gim, '')
+    return ''
+  })
+
+  html = html.replace('</head>', `<style amp-custom data-vue-ssr>${styleConcat}</style></head>`)
+  // Remove every script tag from generated HTML
+  html = html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')
+  // Add AMP script before </head>
+  const ampScript = '<script async src="https://cdn.ampproject.org/v0.js"></script>'
+  html = html.replace('</head>', ampScript + '</head>')
+  return html
+}
+
 export default {
-  mode: 'universal',
-  /*
-  ** Headers of the page
-  */
+  // Headers of the page
   head: {
     title: process.env.npm_package_name || '',
     meta: [
@@ -12,41 +28,25 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'canonical', href: '/' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700%7CFira+Sans:400,700%7CSacramento:400&subset=latin' }
     ]
   },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
+  css: ['~/assets/main.css'],
+  loading: false, // Disable loading bar since AMP will not generate a dynamic page
+  render: {
+    // Disable resourceHints since we don't load any scripts for AMP
+    resourceHints: false
+  },
+  hooks: {
+    // This hook is called before generatic static html files for SPA mode
+    'generate:page': (page) => {
+      page.html = modifyHtml(page.html)
+    },
+    // This hook is called before rendering the html to the browser
+    'render:route': (url, page, { req, res }) => {
+      page.html = modifyHtml(page.html)
     }
   }
 }
